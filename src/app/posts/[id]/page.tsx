a94fe81a690890260cbea38fb92ed825e6fd8dd3
CPGRU <pei19860202@gmail.com>
2024-08-26 10:58:30 +0200

    add font style on blog single page

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,16 @@
 import { Metadata } from "next";
 import Image from "next/image";
+import { Edu_VIC_WA_NT_Beginner } from "next/font/google";
 import { getPostDataById } from "@/app/lib/api"
 
 export const metadata: Metadata = {
     title: 'blog page',
 };
+
+const edu = Edu_VIC_WA_NT_Beginner({
+    subsets: ['latin'],
+    weight: '700',
+})
 
 export default async function Post({params}: { params: {id: string}}){
     const { id, title, postImage, contentHtml} = await getPostDataById(params.id);
@@ -20,7 +26,7 @@ export default async function Post({params}: { params: {id: string}}){
                 <Image src={postImage} alt={id} width={680} height={1020} className="object-cover w-96 h-auto"/>
             </div>
             <div className="flex flex-col ml-10">
-                <div>{title}</div>
+                <div className={`text-2xl text-gray-800  mb-3 ${edu.className}`}>-{title}-</div>
                 <div dangerouslySetInnerHTML={createMarkup()} />  
             </div>
         </div>
